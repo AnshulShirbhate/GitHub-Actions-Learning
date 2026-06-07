b2163ece2b7da98fe0506178516b901c6736ae85
AnshulShirbhate <anshulshirbhate77434@gmail.com>
2026-06-07 23:07:36 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+
+app.get("/", (req, res) => {
+    return res.json({
+        message: "Hello from server!"
+    });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
